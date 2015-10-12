b792cb91fecd2e6ff94b919cf7f4f065939d5027
Keita Ito <keitaitok@gmail.com>
2015-10-11 23:22:47 -0700

    Initial Commit

---
+++ TestProject/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TestProject
+//
+//  Created by Keita Ito on 10/11/15.
+//  Copyright © 2015 Keita Ito. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ TestProject/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  TestProject
+//
+//  Created by Keita Ito on 10/11/15.
+//  Copyright © 2015 Keita Ito. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
